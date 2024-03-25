9297b0b0f6df4c1bc3f304448103abbe74662e26
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-03-25 09:24:30 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/the-25th-annual-putnam-county-spelling-bee/"
 })
